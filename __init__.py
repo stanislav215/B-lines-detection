66f4a49769302a871cc84c1d1ad79202bcaa1695
Stanislav Mačák <stanislav.macak@code2b.com>
2022-03-30 22:59:48 +0200

    fixing

--- __init__.py
+++ __init__.py
@@ -19,7 +19,6 @@ import numpy as np
 from functools import reduce
 import zipfile
 
-"""
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -27,4 +26,3 @@ from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from tensorflow.keras.layers import Conv2D,BatchNormalization,GlobalAveragePooling2D,Dense,MaxPooling2D,InputLayer,Flatten,Dropout
 from tensorflow.keras import Sequential
 
-"""
